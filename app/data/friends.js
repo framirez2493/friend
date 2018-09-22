dd8214bc4e4da5d65faba1ba6aad36c709acc3e8
fernando_R <fer0624@gmail.com>
2018-09-22 13:21:10 -0700

    13

---
+++ app/data/friends.js
@@ -0,0 +1,47 @@
+var friends = [
+    {
+        "name": "Trump",
+        "photo": "public/img/t.jpg",
+        "Nick": "el faviruchis",
+        "scores":  [3, 3, 4, 1, 4, 3, 5, 3, 5, 4]
+    },
+    {
+        "name": "Mistical Taytay",
+        "photo": "http://rizalprovince.ph/tourism_pictures/Avilon%20MZP%20-%20Montalban%20(2).jpg",
+        "Nick": "el faviruchis",
+
+        "scores": [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+    },
+    {
+        "name": "Skylar Rose",
+        "photo": "http://i2.wp.com/northstarz.ca/wp-content/uploads/2013/12/Rose.jpg",
+        "Nick": "el faviruchis",
+
+        "scores": [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+    },
+    {
+        "name": "Colonel Paul",
+        "photo": "http://www.153aw.ang.af.mil/shared/media/bio/Colonel%20Paul%20Lyman.jpg",
+        "Nick": "el faviruchis",
+
+        "scores": [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+    },
+    {
+        "name": "Edward Snowden",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/6/60/Edward_Snowden-2.jpg",
+        "Nick": "el faviruchis",
+
+        "scores": [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+    },
+ 
+    {
+        "name": "Sonneth The Swift",
+        "photo": "http://www.billboard.com/files/styles/article_main_image/public/stylus/1235529-taylor-swift-woman-of-the-year-617-409.jpg",
+        "Nick": "el faviruchis",
+
+        "scores": [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+    }
+
+]
+
+module.exports = friends;
